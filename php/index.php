dfc2d810578d33f5b9475119439042e7cd5d5ab7
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-03 12:18:32 +0100

    kinda finished index.php

--- php/index.php
+++ php/index.php
@@ -86,160 +86,160 @@ if (isset($_POST["theme"]))
             <?php endif; ?>
         </div>
     </nav>
+    <main>
+        <?php
+        if ($session->read('flash')) {
+            echo $session->read('flash');
+            $session->delete('flash');
+        }
+        ?>
 
-    <?php
-    if ($session->read('flash')) {
-        echo $session->read('flash');
-        $session->delete('flash');
-    }
-    ?>
+        <br />
+        <h2>
+            Visualisation de tous les news :
+        </h2>
 
-    <br />
-    <h2>
-        Visualisation de tous les news :
-    </h2>
-
-    <form method="POST">
-        <label for="langueArticle">
-            Trier par langue :
-        </label>
-        <select name="langueArticle" class="button">
-            <option value="0" <?php if (!isset($_POST['langueArticle'])) : ?> selected <?php endif; ?>>
-                TOUTES LES LANGUES
-            </option>
-
-            <?php foreach ($reqLangues as $reqLangue) : ?>
-                <option <?php if (isset($_POST['langueArticle']) && $_POST['langueArticle'] == $reqLangue->id_langue) : ?> selected <?php endif ?> value="<?= $reqLangue->id_langue ?>">
-                    <?= $reqLangue->title ?>
+        <form method="POST">
+            <label for="langueArticle">
+                Filtrer par langue :
+            </label>
+            <select name="langueArticle" class="button">
+                <option value="0" <?php if (!isset($_POST['langueArticle'])) : ?> selected <?php endif; ?>>
+                    TOUTES LES LANGUES
                 </option>
-            <?php endforeach; ?>
-        </select>
 
-        <input class="button" type="submit" name="langue" value="Trier" />
-    </form>
-    <div id="or">
-        <p>OU</p>
-    </div>
+                <?php foreach ($reqLangues as $reqLangue) : ?>
+                    <option <?php if (isset($_POST['langueArticle']) && $_POST['langueArticle'] == $reqLangue->id_langue) : ?> selected <?php endif ?> value="<?= $reqLangue->id_langue ?>">
+                        <?= $reqLangue->title ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
 
-    <form method="POST">
-        <label for="themeArticle">
-            Trier par thème :
-        </label>
-        <select name="themeArticle" class="button">
-            <option value="0" <?php if (!isset($_POST['themeArticle'])) : ?> selected <?php endif; ?>>
-                TOUS LES THEMES
-            </option>
-
-            <?php foreach ($reqThemes as $reqTheme) : ?>
-                <option <?php if (isset($_POST['themeArticle']) && $_POST['themeArticle'] == $reqTheme->id_theme) : ?> selected <?php endif ?> value="<?= $reqTheme->id_theme ?>">
-                    <?= $reqTheme->title ?>
-                </option>
-            <?php endforeach; ?>
-        </select>
-        <input class="button" type="submit" name="theme" value="Trier" />
-    </form>
-
-    <?php  //variable prenant la BdD et appel la fonction query (de la class DataBase pour pouvour sélécionner tous les attributs de la table new)
-
-    if ($reqNews->rowCount()) : //On regarde si il y'a des articles 
-    ?>
-        <div id="articles">
-            <?php
-            foreach ($reqNews as $reqNew) :  //On créé une variable clef reqNew servant d'index pour pouvoir afficher le contenu de la BdD new grâce au foreach
-                $reqTheme = $bdd->query("SELECT * FROM theme WHERE id_theme=?", [$reqNew->id_theme])->fetch(); //variable prenant la BdD et appel la fonction query (de la class DataBase pour pouvour sélécionner tous les attributs de la table new)
-                $reqLangue = $bdd->query("SELECT * FROM langue WHERE id_langue=?", [$reqNew->id_langue])->fetch();
-                $reqEditor = $bdd->query("SELECT surname, name, mail_address FROM editor WHERE id_editor=?", [$reqNew->id_editor])->fetch();
-            ?>
-
-
-                <div class="article">
-                    <!-- < ?= signifie : < ?php echo -->
-                    <p>
-                        <strong>
-                            <u>
-                                L'auteur
-                            </u>
-                            :
-                        </strong>
-                        <?= $reqEditor->surname, " ", $reqEditor->name; ?>
-                        <?php if ($auth->user()) : ?>
-                            <a href="mailto: <?= $reqEditor->mail_address; ?>">
-                                <?= $reqEditor->mail_address; ?>
-                            </a>
-                        <?php endif; ?>
-                    </p>
-
-                    <p>
-                        <strong>
-                            <u>
-                                La langue
-                            </u>
-                            :
-                        </strong>
-                        <?= $reqLangue->title; ?>
-                    </p>
-
-                    <p>
-                        <strong>
-                            <u>
-                                Le titre
-                            </u>
-                            :
-                        </strong>
-                        <?= $reqNew->title_news; ?>
-                    </p>
-
-                    <p title="Description : <?= $reqTheme->description; ?>">
-                        <strong>
-                            <u>
-                                Le theme
-                            </u>
-                            :
-                        </strong>
-                        <?= $reqTheme->title ?>
-                    </p>
-
-                    <p>
-                        <strong>
-                            <u>
-                                Le contenu
-                            </u>
-                            :
-                        </strong>
-                        <?= $reqNew->text_news; ?>
-                    </p>
-
-                    <p>
-                        <strong>
-                            <u>
-                                La date
-                            </u>
-                            :
-                        </strong>
-                        <?= date("d/m/y H:i:s", strtotime($reqNew->date_news)); ?>
-                    </p>
-
-                    <?php if ($auth->user()) {
-                        if ($_SESSION["auth"]->id_editor == $reqNew->id_editor) {
-                    ?> <a href="removeArticle.php?id_news=<?= $reqNew->id_news; ?> "> <button class="removeArticle">Supprimer</button></a> <?php
-
-                                                                                                                                        }
-                                                                                                                                    }
-                                                                                                                                            ?>
-                </div>
-
-            <?php
-            endforeach; //close foreach
-            ?>
+            <input class="info button" type="submit" name="langue" value="Filtrer" />
+        </form>
+        <div id="or">
+            <p>OU</p>
         </div>
-    <?php
-    else :
-    ?>
-        <p>
-            Aucune news enregistrées dans la base de donnée
-        </p> <!-- Dans le cas où il n'y a pas d'article -->
-    <?php endif; ?>
 
+        <form method="POST">
+            <label for="themeArticle">
+                Filtrer par thème :
+            </label>
+            <select name="themeArticle" class="button">
+                <option value="0" <?php if (!isset($_POST['themeArticle'])) : ?> selected <?php endif; ?>>
+                    TOUS LES THEMES
+                </option>
+
+                <?php foreach ($reqThemes as $reqTheme) : ?>
+                    <option <?php if (isset($_POST['themeArticle']) && $_POST['themeArticle'] == $reqTheme->id_theme) : ?> selected <?php endif ?> value="<?= $reqTheme->id_theme ?>">
+                        <?= $reqTheme->title ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <input class="info button" type="submit" name="theme" value="Filtrer" />
+        </form>
+
+        <?php  //variable prenant la BdD et appel la fonction query (de la class DataBase pour pouvour sélécionner tous les attributs de la table new)
+
+        if ($reqNews->rowCount()) : //On regarde si il y'a des articles 
+        ?>
+            <div id="articles">
+                <?php
+                foreach ($reqNews as $reqNew) :  //On créé une variable clef reqNew servant d'index pour pouvoir afficher le contenu de la BdD new grâce au foreach
+                    $reqTheme = $bdd->query("SELECT * FROM theme WHERE id_theme=?", [$reqNew->id_theme])->fetch(); //variable prenant la BdD et appel la fonction query (de la class DataBase pour pouvour sélécionner tous les attributs de la table new)
+                    $reqLangue = $bdd->query("SELECT * FROM langue WHERE id_langue=?", [$reqNew->id_langue])->fetch();
+                    $reqEditor = $bdd->query("SELECT surname, name, mail_address FROM editor WHERE id_editor=?", [$reqNew->id_editor])->fetch();
+                ?>
+
+
+                    <div class="article" style="border-color: <?= "blue" /* couleur du thème de l'article */ ?>;">
+                        <!-- < ?= signifie : < ?php echo -->
+                        <p>
+                            <strong>
+                                <u>
+                                    L'auteur
+                                </u>
+                                :
+                            </strong>
+                            <?= $reqEditor->surname, " ", $reqEditor->name; ?>
+                            <?php if ($auth->user()) : ?>
+                                <a href="mailto: <?= $reqEditor->mail_address; ?>">
+                                    <?= $reqEditor->mail_address; ?>
+                                </a>
+                            <?php endif; ?>
+                        </p>
+
+                        <p>
+                            <strong>
+                                <u>
+                                    La langue
+                                </u>
+                                :
+                            </strong>
+                            <?= $reqLangue->title; ?>
+                        </p>
+
+                        <p>
+                            <strong>
+                                <u>
+                                    Le titre
+                                </u>
+                                :
+                            </strong>
+                            <?= $reqNew->title_news; ?>
+                        </p>
+
+                        <p title="Description : <?= $reqTheme->description; ?>">
+                            <strong>
+                                <u>
+                                    Le theme
+                                </u>
+                                :
+                            </strong>
+                            <?= $reqTheme->title ?>
+                        </p>
+
+                        <p>
+                            <strong>
+                                <u>
+                                    Le contenu
+                                </u>
+                                :
+                            </strong>
+                            <?= $reqNew->text_news; ?>
+                        </p>
+
+                        <p>
+                            <strong>
+                                <u>
+                                    La date
+                                </u>
+                                :
+                            </strong>
+                            <?= date("d/m/y H:i:s", strtotime($reqNew->date_news)); ?>
+                        </p>
+
+                        <?php if ($auth->user()) {
+                            if ($_SESSION["auth"]->id_editor == $reqNew->id_editor) {
+                        ?> <a href="removeArticle.php?id_news=<?= $reqNew->id_news; ?> "> <button class="warning button">Supprimer</button></a> <?php
+
+                                                                                                                                            }
+                                                                                                                                        }
+                                                                                                                                                ?>
+                    </div>
+
+                <?php
+                endforeach; //close foreach
+                ?>
+            </div>
+        <?php
+        else :
+        ?>
+            <p>
+                Aucune news enregistrées dans la base de donnée
+            </p> <!-- Dans le cas où il n'y a pas d'article -->
+        <?php endif; ?>
+    </main>
     <?php
     include_once 'inc/footer.php';
     ?>

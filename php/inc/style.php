04a399757c29a048514f768764462d55bec5c9fe
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-04 17:41:36 +0100

    dark mode colors based on background instead of plain black

--- php/inc/style.php
+++ php/inc/style.php
@@ -9,11 +9,11 @@ $darkMode = isset($_COOKIE["dark_theme"]);
 --main-black: <?= "black" ?>;
 --main-light: <?= "white" ?>;
 --main-dark: <?= "#181818" ?>;
---themed-color: <?= $darkMode ? "#" . getDarkThemeColor("ffa500", "ffffff", "000000") : "#ffa500" ?>;
+--themed-color: <?= $darkMode ? "#" . getDarkThemeColor("ffa500", "ffffff", "303030") : "#ffa500" ?>;
 --themed-white: <?= $darkMode ? "black" : "white" ?>;
 --themed-black: <?= $darkMode ? "white" : "black" ?>;
 --themed-light: <?= $darkMode ? "#303030" : "white" ?>;
 --themed-dark: <?= $darkMode ? "#d0d0d0" : "black"  ?>;
---validate-color: <?= $darkMode ? "#" . getDarkThemeColor("1ED760", "ffffff", "000000") : "#1ED760" ?>;
---cancel-color: <?= $darkMode ? "#" . getDarkThemeColor("D2060D", "ffffff", "000000") : "#D2060D" ?>;
+--validate-color: <?= $darkMode ? "#" . getDarkThemeColor("1ED760", "ffffff", "303030") : "#1ED760" ?>;
+--cancel-color: <?= $darkMode ? "#" . getDarkThemeColor("D2060D", "ffffff", "303030") : "#D2060D" ?>;
 }

5691eebf10c48cc0070a0227f57f238b5a9ac73c
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-04 17:35:07 +0100

    fixed disconnection color

--- php/inc/base.php
+++ php/inc/base.php
@@ -5,7 +5,7 @@ $session = new Session();
 if (isset($_GET["deco"])) { ?>
 
     <div id="deco">
-        <h3>Voulez-vous poursuivre la deconnexion ? </h3>
+        <h3 style="color:white;">Voulez-vous poursuivre la deconnexion ? </h3>
         <p><a href="deconnexion.php">Oui</a></p>
 
         <p><a href="<?= "http://$_SERVER[HTTP_HOST]" . explode('?', $_SERVER['REQUEST_URI'], 2)[0] ?>">Non</a></p>
